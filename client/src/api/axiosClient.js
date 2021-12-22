c6643616c9d57fb14e21bc737111293db9dde5f4
Quan-Kiu <quankiugl@gmail.com>
2021-12-22 15:49:09 +0700

    update

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'https://localhost:5000/',
+    baseURL: 'https://qkmedia.herokuapp.com/',
     headers: {
         'content-type': 'application/json',
     },
