e0a4830e5be88b02ded9f3c6bd0ddb414b853a1d
Quan-Kiu <quankiugl@gmail.com>
2021-12-22 14:01:21 +0700

    update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const { ExpressPeerServer } = require('peer');
 db.connect();
 
 // Middleware
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: `http://localhost:${port}` }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
